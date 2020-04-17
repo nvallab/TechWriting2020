e36136fef1050894e4ae5e6b26ad767bfceaf656
nvallab <56047637+nvallab@users.noreply.github.com>
2020-04-17 15:28:31 -0400

    Add files via upload

---
+++ asteroid.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    if(argc == 2){
+        cout << "Turn on auto-pilot\n";
+    }
+    else
+        cout << "Continue\n";
+    return 0;
+}
